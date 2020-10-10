ab25ec1cb1acab7c8d412a3672a0a91072cb5f95
Carlos Chacin <cchacin@gmail.com>
2020-10-10 12:44:25 -0700

    Finish

--- src/main/java/nomomo/CommitsEndpoint.java
+++ src/main/java/nomomo/CommitsEndpoint.java
@@ -5,14 +5,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Path("commits")
 public class CommitsEndpoint {
 
-    private final CommitsService service;
+    private final Function<String, Optional<Commit>> service;
 
     public CommitsEndpoint(
-            final CommitsService service) {
+            final Function<String, Optional<Commit>> service) {
         this.service = service;
     }
 
@@ -21,6 +23,6 @@ public class CommitsEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     public String getCommit(
             @PathParam("sha") final String sha) {
-        return service.commitBySha(sha).map(Record::toString).orElse("NO COMMIT");
+        return service.apply(sha).map(Record::toString).orElse("NO COMMIT");
     }
 }
